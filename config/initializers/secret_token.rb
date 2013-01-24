3ff27fe61d2ad9751d7774b317f963ea59daf0b2
Bill Ma <billma127@gmail.com>
2013-01-23 22:14:20 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DreamLog::Application.config.secret_token = '3fca3296ad2301f8df6463978c4351f6b29e19d129073e032b16c43d91f760d6796f1bf688f4028af82c3bd50738e97fbec1ea1a75ebfe171bcd008e4043c69b'
