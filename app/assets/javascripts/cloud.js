e032e26e5c55731e7978e592015f20706a95ed03
Bill Ma <billma127@gmail.com>
2013-01-30 19:41:51 -0800

    added logo

--- app/assets/javascripts/cloud.js
+++ app/assets/javascripts/cloud.js
@@ -1,124 +1,118 @@
+var container;
+var camera, scene, renderer;
+var mesh, geometry, material;
+
+var mouseX = 0, mouseY = -10;
+var start_time = Date.now();
+var fps=15
+
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
+
+//init();
+function init() {
+	container = document.createElement( 'div' );
+  container.id="intro"
+	document.body.appendChild( container );
+
+	// Bg gradient
+
+	var canvas = document.createElement( 'canvas' );
+	canvas.width = 32;
+  canvas.height = window.innerHeight;
+
+	var context = canvas.getContext( '2d' );
+
+	var gradient = context.createLinearGradient( 0, 0, 0, canvas.height );
+	gradient.addColorStop(0, "#001621");
+  gradient.addColorStop(0.5, "#080733")
+  gradient.addColorStop(1, "#0A2366");
+
+	context.fillStyle = gradient;
+	context.fillRect(0, 0, canvas.width, canvas.height);
+
+	container.style.background = 'url(' + canvas.toDataURL('image/png') + ')';
+	container.style.backgroundSize = '32px 100%';
 
 
+	camera = new THREE.PerspectiveCamera( 15, window.innerWidth /window.innerHeight, 1, 5000 );
+	camera.position.z = 6000;
 
-			var container;
-			var camera, scene, renderer;
-			var mesh, geometry, material;
+	scene = new THREE.Scene();
 
-			var mouseX = 0, mouseY = -20;
-			var start_time = Date.now();
-      var fps=15
-
-			var windowHalfX = window.innerWidth / 2;
-			var windowHalfY = window.innerHeight / 2;
-
-			init();
-			function init() {
-
-				container = document.createElement( 'div' );
-				document.body.appendChild( container );
-
-				// Bg gradient
-
-				var canvas = document.createElement( 'canvas' );
-				canvas.width = 32;
-				canvas.height = window.innerHeight;
-
-				var context = canvas.getContext( '2d' );
-
-				var gradient = context.createLinearGradient( 0, 0, 0, canvas.height );
-				gradient.addColorStop(0, "#001621");
-        gradient.addColorStop(0.5, "#080733")
-				gradient.addColorStop(1, "#0A2366");
-
-				context.fillStyle = gradient;
-				context.fillRect(0, 0, canvas.width, canvas.height);
-
-				container.style.background = 'url(' + canvas.toDataURL('image/png') + ')';
-				container.style.backgroundSize = '32px 100%';
-
-
-				camera = new THREE.PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 1, 3000 );
-				camera.position.z = 6000;
-
-				scene = new THREE.Scene();
-
-				geometry = new THREE.Geometry();
+	geometry = new THREE.Geometry();
         
-			var texture = THREE.ImageUtils.loadTexture( 'img/cloud_dark.png', null, animate );
-				texture.magFilter = THREE.LinearMipMapLinearFilter;
-				texture.minFilter = THREE.LinearMipMapLinearFilter;
+	var texture = THREE.ImageUtils.loadTexture( 'img/cloud_dark.png', null, animate );
+	texture.magFilter = THREE.LinearMipMapLinearFilter;
+	texture.minFilter = THREE.LinearMipMapLinearFilter;
 
-				var fog = new THREE.Fog( 0x4484E9, - 100, 3000 );
+	var fog = new THREE.Fog( 0x4484E9, - 1000, 3500 );
 
-				material = new THREE.ShaderMaterial( {
+	material = new THREE.ShaderMaterial( {
 
-					uniforms: {
+	 uniforms: {
 
-						"map": { type: "t", value: texture },
-						"fogColor" : { type: "c", value: fog.color },
-						"fogNear" : { type: "f", value: fog.near },
-						"fogFar" : { type: "f", value: fog.far },
+		  "map": { type: "t", value: texture },
+		  "fogColor" : { type: "c", value: fog.color },
+	    "fogNear" : { type: "f", value: fog.near },
+		  "fogFar" : { type: "f", value: fog.far },
 
-					},
-					vertexShader: document.getElementById( 'vs' ).textContent,
-					fragmentShader: document.getElementById( 'fs' ).textContent,
-					depthWrite: false,
-					depthTest: false,
-					transparent: true
+		},
+		vertexShader: document.getElementById( 'vs' ).textContent,
+		fragmentShader: document.getElementById( 'fs' ).textContent,
+		depthWrite: false,
+		depthTest: false,
+		transparent: true
+	} );
 
-				} );
+	var plane = new THREE.Mesh( new THREE.PlaneGeometry( 64, 64 ) );
+	for ( var i = 0; i < 8000; i++ ) {
+		plane.position.x = Math.random() * 1000 - 500;
+		plane.position.y = - Math.random() * Math.random() * 200 - 15;
+		plane.position.z = i;
+	  plane.rotation.z = Math.random() * Math.PI;
+		plane.scale.x = plane.scale.y = Math.random() * Math.random() * 1.5 + 0.5;
+		THREE.GeometryUtils.merge( geometry, plane );
+	}
 
-				var plane = new THREE.Mesh( new THREE.PlaneGeometry( 64, 64 ) );
+	mesh = new THREE.Mesh( geometry, material );
+	scene.add( mesh );
 
-				for ( var i = 0; i < 8000; i++ ) {
-					plane.position.x = Math.random() * 1000 - 500;
-					plane.position.y = - Math.random() * Math.random() * 200 - 15;
-					plane.position.z = i;
-				  plane.rotation.z = Math.random() * Math.PI;
-					plane.scale.x = plane.scale.y = Math.random() * Math.random() * 1.5 + 0.5;
-					THREE.GeometryUtils.merge( geometry, plane );
-				}
+	mesh = new THREE.Mesh( geometry, material );
+	mesh.position.z = - 8000;
+	scene.add( mesh );
 
-				mesh = new THREE.Mesh( geometry, material );
-				scene.add( mesh );
+	renderer = new THREE.WebGLRenderer( { antialias: false } );
 
-				mesh = new THREE.Mesh( geometry, material );
-				mesh.position.z = - 8000;
-				scene.add( mesh );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	container.appendChild( renderer.domElement );
 
-				renderer = new THREE.WebGLRenderer( { antialias: false } );
-				renderer.setSize( window.innerWidth, window.innerHeight );
-				container.appendChild( renderer.domElement );
+	//document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+	window.addEventListener( 'resize', onWindowResize, false );
+}// end of init()
 
-				//document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-				window.addEventListener( 'resize', onWindowResize, false );
 
-			}
+function onDocumentMouseMove( event ) {
+ 		mouseX = ( event.clientX - windowHalfX ) * 0.25;
+		mouseY = ( event.clientY - windowHalfY ) * 0.15;
 
-			function onDocumentMouseMove( event ) {
+}
 
-				mouseX = ( event.clientX - windowHalfX ) * 0.25;
-				mouseY = ( event.clientY - windowHalfY ) * 0.15;
+function onWindowResize( event ) {
+ 		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+		renderer.setSize( window.innerWidth, window.innerHeight );
+}
 
-			}
-
-			function onWindowResize( event ) {
-				camera.aspect = window.innerWidth / window.innerHeight;
-				camera.updateProjectionMatrix();
-				renderer.setSize( window.innerWidth, window.innerHeight );
-			}
-
-			function animate() {
-        setTimeout(function() {
-				requestAnimationFrame( animate );
-				position = ( ( Date.now() - start_time ) * 0.03 ) % 8000;
-				camera.position.x += ( mouseX - camera.position.x ) * 0.01;
-				camera.position.y += ( - mouseY - camera.position.y ) * 0.01;
-				camera.position.z = - position + 8000;
-				renderer.render( scene, camera );
-        }, 1000 / fps);
-
-			}
+function animate() {
+   setTimeout(function() {
+ 		requestAnimationFrame( animate );
+ 		position = ( ( Date.now() - start_time ) * 0.03 ) % 8000;
+  	camera.position.x += ( mouseX - camera.position.x ) * 0.01;
+	  camera.position.y += ( - mouseY - camera.position.y ) * 0.01;
+	  camera.position.z = - position + 8000;
+	  renderer.render( scene, camera );
+   }, 1000 / fps);
+}
 
 		
